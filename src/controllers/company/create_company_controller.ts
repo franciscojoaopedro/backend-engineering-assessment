fde3322590ccdcbef19f1be20b8855ec22a0afe5
franciscojoaopedro <franciscojoaopedro1998@gmail.com>
2024-11-11 19:47:26 +0100

    feat : controller company

---
+++ src/controllers/company/create_company_controller.ts
@@ -0,0 +1,55 @@
+import {FastifyReply, FastifyRequest} from "fastify";
+import create_user_service from "../../core/services/user/create_user_service";
+import {CreateUserDto} from "../../types/entities/user.types";
+import Bcrypt from "../../shared/utils/useBycriptJs";
+import usePrisma from "../../shared/helpers/usePrisma";
+import {ErrorResponse} from "../../shared/utils/errorResponseController";
+
+class Create_user_controller {
+    async execute(request:FastifyRequest,reply:FastifyReply):Promise<FastifyReply> {
+        const {
+            gender,
+            name,
+            password,
+            email,
+            age,
+            country,
+            phone
+        }=request.body as CreateUserDto;
+
+
+        try{
+
+            const {verifyUserExisted}=usePrisma()
+            const {hashPassword}=Bcrypt()
+            if(await verifyUserExisted(email)){
+                return reply.code(200).send({
+                    success:false,
+                    message:"email already exists"
+                })
+            }
+            const user=await create_user_service.execute({
+            email,
+            name,
+            password: await  hashPassword(password),
+            gender,
+            age,
+            country,
+            phone
+        })
+        return  reply.code(200)
+            .send({
+                success:true,
+                message:"user created",
+                data:user
+            });
+        }
+        catch (err){
+             return  ErrorResponse(err,reply)
+        }
+
+    }
+}
+
+const create_user_controller=new Create_user_controller();
+export default (create_user_controller);
---
+++ src/controllers/company/get_all_company_controller.ts
@@ -0,0 +1,26 @@
+import {FastifyReply, FastifyRequest} from "fastify";
+import {ErrorResponse} from "../../shared/utils/errorResponseController";
+import get_all_users_service from "../../core/services/user/get_all_users_service";
+
+
+export class GetAllUserController {
+    async execute(request:FastifyRequest,reply:FastifyReply): Promise<FastifyReply> {
+        try{
+            const users=await get_all_users_service.execute()
+
+          return   reply.code(200)
+                .send({
+                    success:true,
+                    message:"all users",
+                    data:users
+                })
+        }
+        catch(err){
+            return  ErrorResponse(err,reply)
+        }
+    }
+}
+
+
+const get_all_users_controller=new GetAllUserController();
+export default (get_all_users_controller);
