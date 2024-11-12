f8ac93e8fe3ccd8375783f8fe115cd5905909d73
franciscojoaopedro <franciscojoaopedro1998@gmail.com>
2024-11-12 11:40:45 +0100

    feat add filtro

--- src/controllers/job/get_all_jobs_controller.ts
+++ src/controllers/job/get_all_jobs_controller.ts
@@ -4,21 +4,65 @@ import {ErrorResponse} from "../../shared/utils/errorResponseController";
 
 class GetAllJobController {
     async execute(request:FastifyRequest, reply:FastifyReply): Promise<FastifyReply> {
+        const {
+            search,
+            page = 1,
+            limit = 10,
+            jobTitle,
+            companyName
+        } = request.query as any;
 
+        const skip = (page - 1) * limit;
+        const take = Number(limit);
         try
 
         {
-            const jobs =await  prisma.job.findMany({
-               include:{
-                   company:true
-               }
-            })
-            return  reply.status(200)
-                .send({
-                    success:true,
-                    message:"all jobs",
-                    data:jobs
-                })
+
+
+            const where: any = {};
+
+            if (search) {
+                where.OR = [
+                    { title: { contains: search, mode: 'insensitive' } },
+                    { company: { name: { contains: search, mode: 'insensitive' } } }
+                ];
+            }
+
+            if (jobTitle) {
+                where.title = { contains: jobTitle, mode: 'insensitive' };
+            }
+
+            if (companyName) {
+                where.company = { name: { contains: companyName, mode: 'insensitive' } };
+            }
+            
+
+            const jobs = await prisma.job.findMany({
+                where,
+                include: {
+                    company: true
+                },
+                skip,
+                take,
+                orderBy:{
+                    createdAt:"desc"
+                }
+            });
+            const totalJobs = await prisma.job.count({ where });
+
+            return reply.status(200).send({
+                success: true,
+                message: "All jobs retrieved successfully",
+                data: {
+                    jobs,
+                    pagination: {
+                        total: totalJobs,
+                        page: Number(page),
+                        limit: take,
+                        totalPages: Math.ceil(totalJobs / take)
+                    }
+                }
+            });
         }
 
         catch (error) {
